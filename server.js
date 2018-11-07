8141d017bb8a025f05f5e057dff01c56d65d0252
Tom Burgess <tomburgess24@gmail.com>
2018-11-07 18:02:09 +0000

    now globally counts the number of clients in the room

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var http = require("http");
 var readline = require("readline");
 var fs = require("fs");
 
+// show the new user the website
 var server = http.createServer(function(request,response){
 	var markup;
 	fs.readFile("index.html", function(error, markup){
@@ -15,6 +16,7 @@ var server = http.createServer(function(request,response){
 		response.end();
 	});
 });
+// listen for more requests
 server.listen(9000,function(){
 	console.log(new Date() + " listening on port 9000");
 });
@@ -28,44 +30,63 @@ var r1 = readline.createInterface({
 
 var connections = [];
 var connPtr = 0;
-// maintain an array of indexes of the above (connections) variable that indicates if the connection is dead
+// maintain an array of dead connections (indices)
 var deadConnections = [];
 var deadConnPtr = 0;
+var numLive = 0; // number of live clients at any given time
 
 socket.on("request",function(request){
 	var currConn = request.accept(null,request.origin);
-	
+
+	numLive++;
+	console.log(numLive + " clients are live.");
+	// need to tell all clients how many are present
+
 	connections[connPtr] = currConn;
 	connPtr++;
+
+	// inform all users how many are present
+	for (i = 0; i < connections.length; i++){
+		//  sending the data to everyone
+		connections[i].sendUTF("u." + numLive + ".");
+	}
 	
 	console.log(connPtr + " connections have been established.\n");
-
-	
 	
 	currConn.on("close",function(reasonCode,description){
+		numLive--;
 		var identity;
 		for (i = 0; i < connections.length; i++){
+			// update the number of live clients on every client
+			connections[i].sendUTF("u." + numLive + ".");
 			if(connections[i]==this){
 				identity = i;
 			}
 		}
-		console.log(identity + " closed");
+		console.log("Client \"" + identity + "\" closed");
 		deadConnections[deadConnPtr] = identity;
 		deadConnPtr++;
-		console.log("Dead connections are: " + deadConnections);
+		//console.log("Dead connections are: " + deadConnections);
 	});
 	currConn.on("message",function(message){
-		var identity;
-		for (i = 0; i < connections.length; i++){
-			if(connections[i]==this){
-				console.log(i + " said \"" + message.utf8Data + "\"...I think.");
-				identity = i;
+		if (message.utf8Data.includes(",")){
+			// (it's the paintbrush)
+			var identity;
+			for (i = 0; i < connections.length; i++){
+				if(connections[i]==this){
+					console.log(i + " said \"" + message.utf8Data + "\"...I think.");
+					identity = i;
+				}
+			}
+			// send the message back
+			for (i = 0; i < connections.length; i++){
+				//  sending the data to everyone
+				connections[i].sendUTF(message.utf8Data);
 			}
 		}
-		// send the message back
-		for (i = 0; i < connections.length; i++){
-			//  sending the data to everyone
-			connections[i].sendUTF(message.utf8Data);
+		else{
+			// (it's an update to the number of users)
+			
 		}
 	});
 	
